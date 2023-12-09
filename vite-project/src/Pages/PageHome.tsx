3a8ea6f9dc97ba738dc6a528d3a619215cf5654d
vyprodaica02 <duc.luongming@gmail.com>
2023-12-09 16:55:50 +0700

    oke

--- vite-project/src/Pages/PageHome.tsx
+++ vite-project/src/Pages/PageHome.tsx
@@ -2,14 +2,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuth } from "./AuthContext";
+import * as signalR from "@microsoft/signalr";
 interface UserData {
   id: number;
   tenUser: string;
 }
 
+interface Message {
+  content: string;
+}
+
 const PageHome = () => {
   const [userList, setUserList] = useState<UserData[]>([]);
   const { token } = useAuth();
+  const [senUser, setSenUser] = useState<number | undefined>(undefined);
+  const [message, setMessage] = useState("");
+  const [, setMessages] = useState<Message[]>([]);
+  const [listMessage, setListMessage] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -24,7 +33,82 @@ const PageHome = () => {
 
     fetchData();
   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://localhost:7181/api/Chat/GetMessage",
+          {
+            userSenId: token.id,
+            userResenId: senUser,
+          }
+        );
+        setListMessage(response.data);
+      } catch (error) {
+        console.error("Lỗi khi lấy danh sách người dùng", error);
+      }
+    };
 
+    fetchData();
+  }, [senUser]);
+  console.log(listMessage);
+  console.log(token);
+
+  useEffect(() => {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl("/chatHub") // Đường dẫn Hub SignalR
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    connection
+      .start()
+      .then(() => console.log("Connected to SignalR"))
+      .catch((err) => console.error("Error connecting to SignalR:", err));
+
+    connection.on("ReceiveMessage", (message) => {
+      console.log("Received message:", message);
+      // Cập nhật danh sách tin nhắn
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      connection.stop();
+    };
+  }, []);
+
+  const handleSendMessage = async () => {
+    // Gửi tin nhắn thông qua API
+    try {
+      const response = await axios.post(
+        "https://localhost:7181/api/Chat/GuiTinNhan",
+        {
+          content: message,
+          senderUserId: token.id, // Thay thế bằng ID người dùng hiện tại
+          receiverUserId: senUser, // Thay thế bằng ID người dùng nhận
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Message sent successfully");
+      } else {
+        console.error("Failed to send message");
+      }
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+  const handleUserClick = (user: UserData) => {
+    // Lưu thông tin người dùng được chọn vào state
+    setSenUser(user.id);
+    console.log(senUser);
+  };
+
+  const handleKeyDown = (event: { key: string }) => {
+    // Kiểm tra xem phím Enter đã được nhấn
+    if (event.key === "Enter") {
+      handleSendMessage();
+    }
+  };
   return (
     <div className="">
       <div className="flex h-screen">
@@ -33,7 +117,11 @@ const PageHome = () => {
           <h2 className="text-lg font-semibold mb-4">Danh sách Chat</h2>
           <ul>
             {userList.map((_user) => (
-              <li key={_user.id} className="mb-2">
+              <li
+                key={_user.id}
+                className="mb-2 cursor-pointer"
+                onClick={() => handleUserClick(_user)}
+              >
                 <div className="flex items-center">
                   <div className="w-10 h-10 bg-gray-600 rounded-full mr-2"></div>
                   <div>
@@ -67,6 +155,9 @@ const PageHome = () => {
               type="text"
               placeholder="Nhập tin nhắn..."
               className="w-full border rounded-md p-2"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
             />
           </div>
         </div>
